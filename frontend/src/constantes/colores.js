085d22368d7302666f7421995b4506c3c9619e2b
PBurgaleta <92433753+PBurgaleta@users.noreply.github.com>
2026-04-18 13:19:17 +0200

    Add files via upload

---
+++ frontend/src/constantes/colores.js
@@ -0,0 +1,8 @@
+export const colores = [
+  { value: "negro", label: "Negro", color: "#000000" },
+  { value: "blanco", label: "Blanco", color: "#ffffff" },
+  { value: "gris", label: "Gris", color: "#808080" },
+  { value: "rojo", label: "Rojo", color: "#ff0000" },
+  { value: "azul", label: "Azul", color: "#0000ff" },
+  { value: "verde", label: "Verde", color: "#008000" },
+];
