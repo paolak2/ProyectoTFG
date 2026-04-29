eaf76e9e4fca0fc818aab60c1f5d540e4cae7365
PBurgaleta <92433753+PBurgaleta@users.noreply.github.com>
2026-04-29 21:42:15 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
+const initDatabase = require("./initDatabase");
 const app = express();
 app.use(cors());
 app.use(express.json());
